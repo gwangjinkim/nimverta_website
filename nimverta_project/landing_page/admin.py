0cc702e2ca4ed6ceb5bfae066cea1932aa764aad
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-20 18:10:55 +0200

    fix typo

--- nimverta_project/landing_page/admin.py
+++ nimverta_project/landing_page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Page
+from .models import Page, NewsArticle
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
--- nimverta_project/landing_page/urls.py
+++ nimverta_project/landing_page/urls.py
@@ -5,7 +5,7 @@ from . import views  # The '.' here means 'from the current app directory'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('news/', vews.news_list, name='news_list'),
+    path('news/', views.news_list, name='news_list'),
     path('news/<slug:slug>/', views.news_detail, name='news_detail'),
     path('<slug:slug>/', views.page_detail, name='page_detail'),
 ]
