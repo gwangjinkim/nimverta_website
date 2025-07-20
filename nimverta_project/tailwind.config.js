ba400fad2a0e28a4dee3b42ab1c0a9e596d0ae5f
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-20 15:09:04 +0200

    add tailwind config file

---
+++ nimverta_project/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './theme/templates/**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'nimverta-teal': '#2E9A8E', // primary color
+        'nimverta-dark': '#333333', // primary text color
+        'nimverta-light-gray': '#F8F8F8', // background color
+      }
+    },
+  },
+  plugins: [],
+}
