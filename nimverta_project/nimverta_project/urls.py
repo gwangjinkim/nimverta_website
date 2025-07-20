55b2cdaee5b497a5abad53950b7a1479c9e9dfa0
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-20 14:30:33 +0200

    fix nimverta_project/urls.py

--- nimverta_project/nimverta_project/urls.py
+++ nimverta_project/nimverta_project/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
+
+
+
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # for non-'admin/' urls go look for instructions in the 'landing_page.urls' file:
-    path('', views.home, name='home'),
+    path('', include('landing_page.urls')),
 ]
