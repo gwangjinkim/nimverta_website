d9494f53fe9321d6102b0ce86321c01efee9e58e
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-20 14:26:03 +0200

    fix urls

--- nimverta_project/nimverta_project/urls.py
+++ nimverta_project/nimverta_project/urls.py
@@ -14,17 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # When someone visits the base URL (''), run the home view
     path('', views.home, name='home'),
 ]
